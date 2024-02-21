835b2efd491c2e8f99b7e8666e175bc7f6b8a72d
wuvok <tirbyte@gmail.com>
2024-02-21 08:05:30 -0500

    converted functions to subroutines

--- generic.py
+++ generic.py
@@ -6,13 +6,13 @@ from secrets import randbelow, choice
 generic = []
 
 def upperCase():
-    return generic.insert(randbelow(5), choice(ascii_uppercase))
+     generic.insert(randbelow(5), choice(ascii_uppercase))
 def lowCase(): 
-    return generic.insert(randbelow(3), choice(ascii_lowercase))
+     generic.insert(randbelow(3), choice(ascii_lowercase))
 def punctuation(): 
-    return generic.insert(6,choice('!@#@$%&^*'))
+     generic.insert(6,choice('!@#@$%&^*'))
 def numero():
-    return generic.insert(randbelow(7), choice(digits))
+     generic.insert(randbelow(7), choice(digits))
 
 print('write into console ')
 recargar = input()
